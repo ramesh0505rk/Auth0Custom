4f4106699cbf02e0e1bd7a587fb080d680f89231
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-04-06 19:42:37 +0530

    Created Home, Login and Signup components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path:'signup', component: SignupComponent},
+];
